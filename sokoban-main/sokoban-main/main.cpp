952599c2142fe127da04621dec0c99837a69165b
21DP1EShak <101180000+EgorShakirovDP1-1@users.noreply.github.com>
2023-04-05 22:50:40 +0300

    Add files via upload

--- sokoban-main/sokoban-main/main.cpp
+++ sokoban-main/sokoban-main/main.cpp
@@ -11,8 +11,10 @@
 using namespace sf;
 using namespace std;
 
-#define SIZEX 12
-#define SIZEY 12
+int SIZEX = 12;
+int SIZEY = 12;
+#define MAX_SIZEX 48
+#define MAX_SIZEY 48
 #define SIZEF 48
 
 #define WALL 1
@@ -21,7 +23,7 @@ using namespace std;
 #define PLACEBOX 6
 #define EMPTY 0
 
-char board[SIZEX][SIZEY] = {
+char board[MAX_SIZEX][MAX_SIZEY] = {
         { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
         { 1, 0, 0, 0, 0, 4, 4, 4, 0, 0, 0, 1 },
         { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
@@ -36,19 +38,64 @@ char board[SIZEX][SIZEY] = {
         { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
 };
 Vector2i human_vect(6, 9);
+int level = 2;
 
 Sprite box;
 Sprite place;
 Sprite human;
 Sprite wall;
-Sprite pustio;
+Sprite pusto;
 Sprite placebox;
 
 vector <Vector2i> boxes;
 vector <Vector2i> places;
 
-void loadPosition()
+bool readposition(int level)
 {
+    ifstream f("map.txt");
+    if (!f.is_open())
+    {
+        cout << "File map.txt is not found!" << endl;
+        return false;
+    }
+    char s[80];
+    int lev = 0;
+    bool ok = false;
+    f.getline(s, 80);
+    while (!f.eof())
+    {
+        if (s[0] == '#')
+        {
+            lev = atoi(s + 1);
+            if (lev == level)
+            {
+                ok = true;
+                break;
+            }
+        }
+        f.getline(s, 80);
+    }
+    if (!ok)
+    {
+        return false;
+    }
+    f >> SIZEX >> SIZEY;
+    f >> human_vect.x >> human_vect.y;
+    for (size_t i = 0; i < SIZEY; i++)
+    {
+        for (size_t j = 0; j < SIZEX; j++)
+        {
+            f >> board[j][i];
+            board[j][i] -= '0';
+        }
+    }
+
+    f.close();
+    return true;
+}
+void loadPosition(int level)
+{
+    readposition(level);
     for (int i = 0; i < SIZEX; i++)
     {
         for (int j = 0; j < SIZEY; j++)
@@ -70,7 +117,7 @@ void loadPosition()
             }
             else
             if (board[i][j] == 0)
-                pustio.setPosition(i * SIZEF, j * SIZEF);
+                pusto.setPosition(i * SIZEF, j * SIZEF);
             else
             if (board[i][j] == 6)
                 placebox.setPosition(i * SIZEF, j * SIZEF);
@@ -120,24 +167,24 @@ bool no_free_boxes()
     return true;
 }
 
-int main(int argv, char *argc[])
+void work(int level)
 {
-
+    loadPosition(level);
     RenderWindow window(VideoMode(SIZEX * SIZEF, SIZEY * SIZEF), "Sokoban!");
     window.setFramerateLimit(60);
 
     Texture t2, t7;
     t2.loadFromFile("images/background.jpg");
     t7.loadFromFile("images/figures.png");
+    //window.setSize(Vector2u(SIZEX * SIZEF, SIZEY * SIZEF));
 
     box.setTexture(t7);		box.setTextureRect(IntRect(0, 0, SIZEF, SIZEF));
     place.setTexture(t7);   place.setTextureRect(IntRect(SIZEF, 0, SIZEF, SIZEF));
     human.setTexture(t7);   human.setTextureRect(IntRect(2 * SIZEF, 0, SIZEF, SIZEF));
     wall.setTexture(t7);    wall.setTextureRect(IntRect(3 * SIZEF, 0, SIZEF, SIZEF));
-    pustio.setTexture(t7);   pustio.setTextureRect(IntRect(4 * SIZEF, 0, SIZEF, SIZEF));
+    pusto.setTexture(t7);   pusto.setTextureRect(IntRect(4 * SIZEF, 0, SIZEF, SIZEF));
     placebox.setTexture(t7);   placebox.setTextureRect(IntRect(5 * SIZEF, 0, SIZEF, SIZEF));
 
-    loadPosition();
 
     //int even = 0;
     while (window.isOpen())
@@ -147,7 +194,7 @@ int main(int argv, char *argc[])
         {
             if (e.type == Event::Closed)
                 window.close();
-            if (Keyboard::isKeyPressed(Keyboard::Escape) )
+            if (Keyboard::isKeyPressed(Keyboard::Escape))
             {
                 window.close();
             }
@@ -193,8 +240,8 @@ int main(int argv, char *argc[])
                 else
                 if (board[i][j] == 0)
                 {
-                    pustio.setPosition(i * SIZEF, j * SIZEF);
-                    window.draw(pustio);
+                    pusto.setPosition(i * SIZEF, j * SIZEF);
+                    window.draw(pusto);
                 }
                 else
                 if (board[i][j] == 6)
@@ -209,8 +256,17 @@ int main(int argv, char *argc[])
         if (no_free_boxes())
         {
             cout << "You win!!!" << endl;
+            return;
         }
         window.display();
     }
+}
+int main(int argv, char *argc[])
+{
+    for (int level = 0; level <= 3; level ++)
+    {
+        work(level);
+    }
+
     return 0;
 }
