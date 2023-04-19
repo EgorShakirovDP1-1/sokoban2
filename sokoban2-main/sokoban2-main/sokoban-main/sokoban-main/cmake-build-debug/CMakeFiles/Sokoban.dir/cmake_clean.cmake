bd5aae8762dea07121c0f04a6ad030fb8f9f162c
21DP1EShak <101180000+EgorShakirovDP1-1@users.noreply.github.com>
2023-04-19 22:25:06 +0300

    Add files via upload

---
+++ sokoban2-main/sokoban2-main/sokoban-main/sokoban-main/cmake-build-debug/CMakeFiles/Sokoban.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sokoban.dir/main.cpp.obj"
+  "CMakeFiles/Sokoban.dir/main.cpp.obj.d"
+  "Sokoban.exe"
+  "Sokoban.exe.manifest"
+  "Sokoban.pdb"
+  "libSokoban.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sokoban.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
